d8eb59b0ab93eb7606b3e986e9eb686b0696e218
Gopal Nambiar <gopalnambiar2@gmail.com>
2024-03-01 02:39:08 -0800

    Homebrew files check in

---
+++ Formula/res_cli.rb
@@ -0,0 +1,19 @@
+class ResCli < Formula
+    desc "CLI to manage ResilientDB and Python SDK instances"
+    homepage "https://github.com/gopuman/homebrew-ResCLI"
+    url "https://github.com/ResilientApp/ResCLI/archive/refs/tags/nightly.tar.gz"
+    sha256 "11f3d8b5c0864a9d22a9c07ebd39beaac54ecd3677e3b6f262c13162e0871d47"
+    version "nightly"
+  
+    depends_on "ruby"
+  
+    def install
+      libexec.install Dir["lib/*"]
+      bin.install "#{libexec}/res_cli/cli.rb" => "res_cli"
+    end
+  
+    test do
+      system "#{bin}/res_cli", "--version"
+    end
+  end
+  
---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+gem 'open3'
+gem 'optparse'
+gem 'inifile'
+gem 'rake'
